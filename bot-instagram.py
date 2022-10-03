e99295f1580b32f7c7804db9d43b4e36eb7f3d99
Delfim dos Anjos Fumane <81496281+Delfim-dos-Anjos@users.noreply.github.com>
2022-10-04 00:59:03 +0200

    Update bot-instagram.py

--- bot-instagram.py
+++ bot-instagram.py
@@ -1,8 +1,8 @@
 from xml.etree.ElementTree import Comment
 from selenium import webdriver
-from seleniun.webdriver.common.keys import keys
+from selenium.webdriver.common.keys import keys
 from time import sleep
-from ramdom import randint
+from random import randint
 
 chromedrive_path = "C:\Users\Delfim dos Anjos\Downloads\Compressed\chromedriver/chromedriver.exe"
 webdriver = webdriver.Chrome(executable_path=chromedrive_path)
@@ -43,9 +43,55 @@ for hashtag in hashtag_list:
     primeira_thumb = webdriver.find_element_by_xpath(
         '/html/body/div[1]/section/main/article/div[1]/div/div/div[1]/div[1]/a/div/div[2]')
     primeira_thumb.click()
-    sleep(randint(2, 3))
-    try:
-        for _ in range(1, 6):
+    sleep(randint(3, 4))
 
-        except:
-            continue
+    #iniciio do programa
+    try:
+        for _ in range(1, 6): #usei o `_` no lugar da var  porque ñ vou usar a var, porque eu só quero que faça o for pela quantidade de seguidores
+            usuario = webdriver.find_element_by_xpath('//*[@id="mount_0_0_pA"]/div/div/div/div[2]/div/div/div[1]/div/div[3]/div/div/div/div/div[2]/div/article/div/div[2]/div/div/div[1]/div/header/div[2]/div[1]/div[1]/div/div/div/span/a').text
+
+            if usuario not in novos_users_seguidos:
+                if webdriver.find_element_by_xpath('/html/body/div[1]/div/div/div/div[2]/div/div/div[1]/div/div[3]/div/div/div/div/div[2]/div/article/div/div[2]/div/div/div[1]/div/header/div[2]/div[1]/div[2]/button/div/div').text == 'seguir':
+                    webdriver.find_element_by_xpath('/html/body/div[1]/div/div/div/div[2]/div/div/div[1]/div/div[3]/div/div/div/div/div[2]/div/article/div/div[2]/div/div/div[1]/div/header/div[2]/div[1]/div[2]/button/div/div').click()
+                    novos_users_seguidos.append(usuario)
+                    seguindo = seguindo + 1
+
+                    #like
+                    button_like = webdriver.find_element_by_xpath('/html/body/div[1]/div/div/div/div[2]/div/div/div[1]/div/div[3]/div/div/div/div/div[2]/div/article/div/div[2]/div/div/div[2]/section[1]/span[1]/button/div[2]/span/svg')
+                    button_like.click()
+                    likes = likes + 1
+                    sleep(randint(3, 4))
+
+                    #comentario
+                    webdriver.find_element_by_xpath('/html/body/div[1]/div/div/div/div[2]/div/div/div[1]/div/div[3]/div/div/div/div/div[2]/div/article/div/div[2]/div/div/div[2]/section[1]/span[2]/button').click()
+                    comment_box = webdriver.find_element_by_xpath('/html/body/div[1]/div/div/div/div[2]/div/div/div[1]/div/div[3]/div/div/div/div/div[2]/div/article/div/div[2]/div/div/div[2]/section[3]/div/form/textarea')
+
+                    com = randint(1, 10)
+                    if com < 6:
+                        comment_box.send_keys('Nice!')
+                        sleep(3)
+                    elif com > 5 and com < 9:
+                        comment_box.send_keys('Muito boomm!')
+                        sleep(3)
+                    elif com == 9:
+                        comment_box.send_keys('Bom trabalho')
+                        sleep(3)
+                    elif com == 10:
+                        comment_box.send_keys('Soft!!')
+                        sleep(3)
+
+                        comment_box.send_keys(keys.ENTER)
+                        comentarios = comentarios +1
+                        sleep(randint(2, 4))
+
+                        webdriver.find_element_by_link_text('Seguinte').click()
+                        sleep(randint(2, 4))
+                    else:
+                        webdriver.find_element_by_link_text('Seguinte').click()
+                        
+    except:
+        continue
+
+print('Liked {} fotos.'.format(likes))
+print('Comentarios {} fotos.'.format(comentarios))
+print('Seguindo {} novas pessoas.'.format(seguindo))
